61061c5e210dd1783c1f01b5f73e55606ebc6b31
Matt <mattsdni@gmail.com>
2016-04-19 20:29:59 -0700

    implemented add student account

--- src/TestUtilities.java
+++ src/TestUtilities.java
@@ -33,12 +33,7 @@ public class TestUtilities {
 
             switch (choice) {
                 case 0: callOpenDB();	break;
-                case 1: callOpenDBUserPass();	break;
-                case 2: callWorksOnProjectInDepartment(); break;
-                case 3: callProjectStats(); break;
-                case 4: callWorksOnProjWith(); break;
-                case 5: callEmpWithOutWork(); break;
-                case 6: callUpdateWorksOn(); break;
+                case 1: callAddStudentAccount(); break;
                 case 8: callCloseDB(); break;
                 default: System.out.println("Method not implemented at this time");
             }// switch
@@ -53,13 +48,7 @@ public class TestUtilities {
 
     static void displaymenu() {
         System.out.println("0)  openDB()");
-        System.out.println("1)  openDB(user, pass, dbName)");
-        System.out.println("2)  WorksOnProjectInDepartment(projName)");
-        System.out.println("3)  ProjectStats()");
-        System.out.println("4)  WorksOnProjWith(empFname, empLname) ");
-        System.out.println("5)  EmployeeWithoutWork(First, Last Name) ");
-        System.out.println("6)  UpdateWorksOn(data) ");
-        System.out.println("7)  othermethods");
+        System.out.println("1)  callAddStudentAccount() ");
         System.out.println("8)  close the DB");
         System.out.println("9)  quit");
     }
@@ -90,20 +79,6 @@ public class TestUtilities {
         System.out.println(testObj.getConn());
     }
 
-    // open the default open database;
-    public static void callOpenDBUserPass() {
-        System.out.print("The value of testObj.conn before opening the DB: ");
-        System.out.println(testObj.getConn());
-        System.out.print("Enter database username: ");
-        String username = keyboard.nextLine();
-        System.out.print("Enter database password: ");
-        String password = keyboard.nextLine();
-        testObj.openDB(username, password);
-        System.out.print("The value of testObj.conn after opening the DB: ");
-        System.out.println(testObj.getConn());
-    }
-
-
     // close the database;
     public static void callCloseDB() {
         System.out.print("The value of testObj.conn before closing the DB: ");
@@ -113,78 +88,19 @@ public class TestUtilities {
         System.out.println(testObj.getConn());
     }
 
-    public static void callWorksOnProjectInDepartment()
+
+    public static void callAddStudentAccount()
     {
-        System.out.println("Enter a project name: ");
-        int dnum;
-        try
-        {
-            dnum = Integer.parseInt(keyboard.nextLine());
-            printResultSet(testObj.worksOnProjectInDepartment(dnum));
-        }catch (NumberFormatException e)
-        {
-            System.out.println("Department number must be an integer...");
-        }
-    }
-
-    public static void callProjectStats()
-    {
-        printResultSet(testObj.projectStats());
-    }
-
-    public static void callEmpWithOutWork()
-    {
-        printResultSet(testObj.empWithOutWork());
-    }
-
-    public static void callWorksOnProjWith()
-    {
-        System.out.print("Enter first name: ");
-        String fname = keyboard.nextLine();
-        System.out.print("Enter last name: ");
-        String lname = keyboard.nextLine();
-        printResultSet(testObj.worksOnProjWith(fname, lname));
-    }
-
-    public static void callMatchLastName()
-    {
-        printResultSet(testObj.matchLastName("Kin"));
-    }
-
-    public static void callUpdateWorksOn()
-    {
-        String cont = "y";
-        LinkedList<String[]> data = new LinkedList<>();
-        String[] row;
-        while (cont.contains("y"))
-        {
-            System.out.print("Enter SSN: ");
-            row = new String[3];
-            row[0] = keyboard.nextLine();
-
-            System.out.print("Enter Project Number: ");
-            row[1] = keyboard.nextLine();
-
-            System.out.print("Enter Hours: ");
-            row[2] = keyboard.nextLine();
-
-            data.add(row);
-
-            System.out.println("Add another row? ");
-            cont = keyboard.nextLine();
-        }
-        String[][] data_array = new String[data.size()][3];
-        for (int i = 0; i < data_array.length; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                data_array[i][j] = data.get(i)[j];
-            }
-        }
-        int result = testObj.updateWorksOn(data_array);
-
+        System.out.print("Enter name: ");
+        String name = keyboard.nextLine();
+        System.out.print("Enter password: ");
+        String password = keyboard.nextLine();
+        System.out.print("Enter email: ");
+        String email = keyboard.nextLine();
+        int result = testObj.addStudentAccount(name, password, email);
         System.out.println(result + " row" + (result > 1 ? "s were" : " was") + " affected") ;
     }
+
 
     private static void printResultSet(ResultSet rs)
     {
--- src/Utilities.java
+++ src/Utilities.java
@@ -39,9 +39,8 @@ public class Utilities {
     public void openDB(String username, String password)
     {
         // Database Info
-        //String url = "jdbc:mysql://zoe.cs.plu.edu:3306/";
         String url = "jdbc:mysql://zoe.cs.plu.edu:3306/";
-        String db = "company367_2016";
+        String db = "dt367_2016";
 
         if (username == null) username = "dt367";
         if (password == null) password = "dt367";
@@ -71,229 +70,74 @@ public class Utilities {
         }
     }
 
-    /**
-     * This method creates an SQL statement to list fname, lname, salary of all
-     * employees that work in the department with dname='Research'
-     *
-     * @return ResultSet that contains three columns lname, fname, salary of
-     * all employees that work in the research department
-     */
-    public ResultSet getNameSalary() {
-        ResultSet rset = null;
-        String sql = null;
 
-        try {
-            // create a Statement and an SQL string for the statement
-            Statement stmt = conn.createStatement();
-            sql = "SELECT lname, fname, salary FROM employee, department " +
-                    "WHERE dno=dnumber and dname='Research' " +
-                    "ORDER BY lname, fname";
-            rset = stmt.executeQuery(sql);
-        } catch (SQLException e) {
-            System.out.println("createStatement " + e.getMessage() + sql);
-        }
 
-        return rset;
-    }// getNameSalary
-
-    /**
-     * This method creates an SQL statement to list fname, lname, and department
-     * number of all employees that have a last name that starts with
-     * the String target
-     *
-     * @param target the string used to match beginning of employee's last name
-     * @return ResultSet that contains lname, fname, and department number of all
-     * employees that have a first name that starts with target.
-     */
-    public ResultSet matchLastName(String target)
-    {
-        ResultSet rset = null;
-        String sql = null;
-
-        try {
-            // create a Statement and an SQL string for the statement
-            Statement stmt = conn.createStatement();
-            sql = "SELECT dno, lname, fname FROM employee " +
-                    "WHERE lname like '"+target+"%' " +
-                    "ORDER BY dno";
-            rset = stmt.executeQuery(sql);
-        } catch (SQLException e) {
-            System.out.println("createStatement " + e.getMessage() + sql);
-        }
-
-        return rset;
-    }
-
-    /**
-     * This method creates an SQL statement to list fname, lname, and department
-     * number of all employees that work in the department with number
-     * dno
-     *
-     * @param dno the department number
-     * @return ResultSet that contains lname, fname, and department number of all
-     * employees that work in the department number dno
-     */
-    //EXAMPLE OF USING A PreparedStatement AND SETTING Parameters
-    public ResultSet employeeByDNO(int dno) {
-        ResultSet rset = null;
-        String sql = null;
-
-        try {
-            // create a Statement and an SQL string for the statement
-            sql = "SELECT dno, lname, fname FROM employee " +
-                    "WHERE dno = ? " +
-                    "ORDER BY dno";
-            PreparedStatement pstmt = conn.prepareStatement(sql);
-
-            pstmt.clearParameters();
-            pstmt.setInt(1,dno); //set the 1 parameter
-
-            rset = pstmt.executeQuery();
-        } catch (SQLException e) {
-            System.out.println("createStatement " + e.getMessage() + sql);
-        }
-
-        return rset;
-    }// matchLastName2
-
-    /******************** 2 Write and Test ********************************
-     * Write a method that returns lname, fname, project number and hours of
-     * all employees that work on a project controlled by department, deptNum.
-     * Here deptNum is given as input from the client
-     *
-     * @param deptNum is the controlling department number
-     * @return ResultSet with lname, fname, project number and hours of
-     * all employees that work on a project controlled by department dno
-     */
-    public ResultSet worksOnProjectInDepartment(int deptNum)
-    {
-//        select lname, fname, project.pnumber, hours
-//        from employee, works_on, project
-//        where ssn=works_on.essn and project.dnum=5
-        try
-        {
-            PreparedStatement q = conn.prepareStatement("");
-            return q.executeQuery();
-        } catch (SQLException e)
-        {
-            e.printStackTrace();
-            return null;
-        }
-    }
-
-    /******************** 3 Write and Test ********************************
-     * Write a method that returns for each project the number of employees that
-     * work on the project, the total number of hours they have all worked on the
-     * project, and the average number of hours each employee has worked on the
-     * project.
-     *
-     * @return ResultSet that has for each project the number of employees that
-     * work on the project, the total number of hours they have all worked on the
-     * project, and the average number of hours each employee has worked on project
-     */
-    public ResultSet projectStats()
+    public int addStudentAccount(String name, String password, String email)
     {
         try
         {
-            PreparedStatement q = conn.prepareStatement("SELECT p.pname, count(*) AS num_employees, sum(w.hours) AS total_hours, avg(w.hours) AS avg_hours " +
-                    "FROM employee e, works_on w, project p " +
-                    "WHERE e.ssn=w.essn AND w.pno = p.pnumber " +
-                    "GROUP BY p.pname");
-            return q.executeQuery();
+            PreparedStatement q = conn.prepareStatement("insert into student (name, password, email) " +
+                    "VALUES (?, ?, ?)");
+            q.setString(1, name);
+            q.setString(2, password);
+            q.setString(3, email);
+            return q.executeUpdate();
         } catch (SQLException e)
         {
             e.printStackTrace();
-            return null;
+            return 0;
         }
     }
 
-    /******************** 4 Write and Test ********************************
-     * Write a method that returns fname, lname, salary, and dno for each employee
-     * that works on a project with the employee specified by input values
-     * empFname, empLname
-     *
-     * @param empFname is the first name of the employee
-     * @param empLname is the last name of the employee
-     * @return ResultSet that has fname, lname, salary, and dno for each employee
-     * that works on a project with the employee empFname, empLname
-     */
-    public ResultSet worksOnProjWith(String empFname, String empLname)
+    public int addSchedule(String email, String degree, String starting_semester, String starting_year, String name)
     {
         try
         {
-            PreparedStatement q = conn.prepareStatement("select distinct concat(fname, ' ', lname) as name, salary, dno " +
-                    "from employee, works_on " +
-                    "where ssn=essn and pno in " +
-                    "(select pno from employee, works_on where ssn=essn and fname=? and lname=?)");
-            q.setString(1, empFname);
-            q.setString(2, empLname);
-            return q.executeQuery();
+            PreparedStatement q = conn.prepareStatement("INSERT INTO schedule (student_email, degree, starting_semester, starting_year, name) " +
+                    "VALUES (?,?,?,?,?)");
+            q.setString(1, email);
+            q.setString(2, degree);
+            q.setString(3, starting_semester);
+            q.setString(4, starting_year);
+            q.setString(5, name);
+            return q.executeUpdate();
         } catch (SQLException e)
         {
             e.printStackTrace();
-            return null;
+            return 0;
         }
     }
 
-    /******************** 5 Write and Test ********************************
-     * Retrieve the names of employees who do not work on any project and their salary
-     * Names must be in the format "lname, fname" i.e., the last name and first name
-     * must be concatenated.
-     *
-     * @return ResultSet that has employee name and salary of all employees that do
-     *         not work on any project.
-     */
-    public ResultSet empWithOutWork()
+    public int addAdvisor(String student_email, String advisor_email)
     {
         try
         {
-            PreparedStatement q = conn.prepareStatement("SELECT concat(e.fname, ' ',e.lname) as name, salary " +
-                    "FROM employee e " +
-                    "LEFT JOIN works_on w " +
-                    "ON e.ssn=w.essn " +
-                    "WHERE w.essn IS NULL");
-            return q.executeQuery();
+            PreparedStatement q = conn.prepareStatement("update student set advisor_email = ? " +
+                    "where email = ?");
+            q.setString(1, advisor_email);
+            q.setString(2, student_email);
+            return q.executeUpdate();
         } catch (SQLException e)
         {
             e.printStackTrace();
-            return null;
+            return 0;
         }
     }
 
-
-    /******************** 6  Write and Test ********************************
-     * YOU MUST USE A PreparedStatement
-     * This method will use a PreparedStatement and the information in data
-     * to update the works_on table.  Each row of the 2-dim array, data,
-     * contains the 3 attributes for one tuple in the works_on
-     * table. The 2-dim array is a nx3 array and the column format is (essn, pno, hours)
-     * The method returns the number of tuples successfully inserted.
-     *
-     * @param data is a nx3 table of Strings where each row has the format (essn, pno, hours)
-     * @return number of tuples successfully inserted into works_on
-     */
-    public int updateWorksOn(String[][] data)
+    public int deleteCourse(String dept, String course_number)
     {
-        int result = 0;
-        for (String[] row : data)
+        try
         {
-            try
-            {
-                PreparedStatement q = conn.prepareStatement("INSERT INTO works_on (essn, pno, hours) VALUES (?,?,?) " +
-                        "ON DUPLICATE KEY UPDATE " +
-                        "essn = VALUES(essn), " +
-                        "pno = VALUES(pno), " +
-                        "hours = VALUES(hours)");
-                q.setString(1, row[0]);
-                q.setString(2, row[1]);
-                q.setString(3, row[2]);
-                result += q.executeUpdate();
-            } catch (SQLException e)
-            {
-                e.printStackTrace();
-            }
+            PreparedStatement q = conn.prepareStatement("DELETE FROM course " +
+                    "WHERE department = ? AND course_number = ?");
+            q.setString(1, dept);
+            q.setString(2, course_number);
+            return q.executeUpdate();
+        } catch (SQLException e)
+        {
+            e.printStackTrace();
+            return 0;
         }
-        return result;
     }
 
     /**
